d45280361648d289d43687679e99425dcfb7243d
Sergey Lunev <sergey@lunyov.ru>
2022-03-13 10:53:11 +0300

    HW_02: String unpacking is ready

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var result strings.Builder
+
+	var previous rune = 0
+
+	for i, r := range input {
+		if i == 0 && unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(r) {
+			if previous == 0 {
+				return "", ErrInvalidString
+			}
+
+			repeat, _ := strconv.Atoi(string(r))
+			result.WriteString(strings.Repeat(string(previous), repeat))
+
+			previous = 0
+		} else {
+			if previous != 0 {
+				result.WriteRune(previous)
+			}
+			previous = r
+		}
+	}
+
+	if previous != 0 {
+		result.WriteRune(previous)
+	}
+
+	return result.String(), nil
 }
