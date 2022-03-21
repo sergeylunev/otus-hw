f19cae8487d7fe7da1d17e2e2521934ade4c082e
Sergey Lunev <sergey@lunyov.ru>
2022-03-21 20:23:06 +0300

    HW03: Frequency analysis done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,50 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordCount struct {
+	Word  string
+	Count int
+}
+
+func Top10(input string) []string {
+	if input == "" {
+		return []string{}
+	}
+	words := strings.Fields(input)
+
+	wordsCountMap := make(map[string]int)
+	for _, word := range words {
+		wordsCountMap[word] = wordsCountMap[word] + 1
+	}
+
+	wordsResultCountSlice := make([]WordCount, 0, len(wordsCountMap))
+	for word, count := range wordsCountMap {
+		wordsResultCountSlice = append(wordsResultCountSlice, WordCount{
+			Word:  word,
+			Count: count,
+		})
+	}
+
+	sort.SliceStable(wordsResultCountSlice, func(i, j int) bool {
+		return wordsResultCountSlice[i].Word < wordsResultCountSlice[j].Word
+	})
+	sort.SliceStable(wordsResultCountSlice, func(i, j int) bool {
+		return wordsResultCountSlice[i].Count > wordsResultCountSlice[j].Count
+	})
+
+	length := len(wordsResultCountSlice)
+	if length > 10 {
+		length = 10
+	}
+
+	result := make([]string, length, length)
+	for i, word := range wordsResultCountSlice[:length] {
+		result[i] = word.Word
+	}
+
+	return result
 }
