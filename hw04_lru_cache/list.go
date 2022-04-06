a926737251edd3bdbb8c9b61b0523b62654cc310
Sergey Lunev <sergey@lunyov.ru>
2022-04-07 00:54:44 +0300

    HW_04: Start working with List implementation

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,9 +17,48 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	first  *ListItem
+	last   *ListItem
+	length int
 }
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	li := ListItem{Value: v}
+
+	if l.Len() == 0 {
+		l.first = &li
+		l.last = &li
+	} else {
+		li.Next = l.first
+		l.first.Prev = &li
+
+		l.first = &li
+	}
+
+	l.length++
+
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	return l.last
+}
+
+func (l *list) MoveToFront(i *ListItem) {}
+
+func (l *list) Remove(i *ListItem) {}
 
 func NewList() List {
 	return new(list)
