e945729d71486a233aa2396a6840e509ba139c53
Sergey Lunev <sergey@lunyov.ru>
2022-05-16 23:21:32 +0300

    HW_06: Pipline execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,34 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	for _, stage := range stages {
+		if stage != nil {
+			out = execute(stage, done, out)
+		}
+	}
+
+	return out
+}
+
+func execute(stage Stage, done In, out In) Out {
+	result := make(Bi)
+
+	go func() {
+		defer close(result)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-out:
+				if !ok {
+					return
+				}
+				result <- v
+			}
+		}
+	}()
+
+	return stage(result)
 }
